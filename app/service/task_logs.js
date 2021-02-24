b125bb24bded7b2bf8912a5700585dd5cbc790a0
Imfdj <907765782@qq.com>
2021-02-24 21:54:03 +0800

    feat: 完善任务日志；完善修改任务接口；
    完善任务日志；完善修改任务接口；

--- app/service/task_logs.js
+++ app/service/task_logs.js
@@ -6,11 +6,12 @@ const { Op } = require('sequelize');
 class _objectName_Service extends Service {
   async findAll(payload) {
     const { ctx } = this;
-    const { limit, offset, prop_order, order, remark, task_id } = payload;
+    const { limit, offset, prop_order, order, remark, task_id, is_comment } = payload;
     const where = {};
     const Order = [];
     remark ? (where.remark = { [Op.like]: `%${ remark }%` }) : null;
     !ctx.helper.tools.isParam(task_id) ? where.task_id = task_id : null;
+    !ctx.helper.tools.isParam(is_comment) ? where.is_comment = is_comment : null;
     prop_order && order ? Order.push([prop_order, order]) : null;
     return await ctx.model.TaskLogs.findAndCountAll({
       limit,
